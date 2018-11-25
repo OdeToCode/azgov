4f4fcb009ebb15b27ac8f2c64245c47dac6734ee
Scott Allen <scott@odetocode.com>
2018-11-25 12:00:10 -0500

    tweaking cost reports

--- processing/AzGovenorProcessing/GovenorReports/Pages/Cost.cshtml.cs
+++ processing/AzGovenorProcessing/GovenorReports/Pages/Cost.cshtml.cs
@@ -19,11 +19,13 @@ namespace GovenorReports.Pages
 
         public Run Run { get; private set; }
         public IList<CostReport> Costs { get; private set; }
+        public IEnumerable<IGrouping<string, CostReport>> CostsByGroup { get; private set; }
 
         public void OnGet()
         {
             Run = queries.GetLastRun();
             Costs = queries.GetCostReports(Run.RunID);
+            CostsByGroup = Costs.GroupBy(c => c.GroupName.ToLower()).OrderByDescending(g => g.Sum(r => r.MonthlyCost));
         }
     }
 }
