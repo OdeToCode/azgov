d992712f9d65f9fa5d5ec9b4f1ecaa5fc564b64d
Scott Allen <scott@odetocode.com>
2018-11-25 18:55:07 -0500

    cost report tweaks

--- processing/AzGovenorProcessing/GovenorReports/Pages/Cost.cshtml.cs
+++ processing/AzGovenorProcessing/GovenorReports/Pages/Cost.cshtml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using GovenorReports.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GovenorReports.Pages
